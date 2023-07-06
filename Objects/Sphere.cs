2199679a7083c727a6a20e7736fa4d784cced48e
BenApplegate <applegateben03@gmail.com>
2023-07-06 16:32:00 -0600

    Added template Sphere class

--- Objects/Sphere.cs
+++ Objects/Sphere.cs
@@ -1,6 +1,25 @@
-﻿namespace Raytracer.Objects;
+﻿using System.Numerics;
+using Raytracer.Core;
+using Raytracer.Interfaces;
 
-public class Sphere
+namespace Raytracer.Objects;
+
+public class Sphere : Renderable
 {
+    private Vector3 location;
+    private float radius;
+
+    public Sphere(Vector3 location, float radius)
+    {
+        this.location = location;
+        this.radius = radius;
+    }
+    public RayHit Render(Ray ray)
+    {
+        Vector3 rayOrigin = ray.origin - location;
+
+
+        return new RayHit() { didHit = false, hitLocation = Vector3.Zero, distance = 0, hitNormal = Vector3.Zero };
+    }
     
 }
